84e9b7c815dfaaa4accffca2d5df7467a12064d4
LCR <994094256@qq.com>
2019-04-25 11:30:12 +0800

    fix

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -38,16 +38,16 @@ class EasySwooleEvent implements Event
             if ($workerId == 0) {
 
                 Timer::getInstance()->loop(10 * 1000, function () {
-                    echo 'aaaaa';echo time();echo '\n';
+                    echo 'aaaaa';echo time();echo "\n";
                     // 从数据库，或者是redis中，去获取下个就近10秒内需要执行的任务
                     // 例如:2秒后一个任务，3秒后一个任务 代码如下
                     Timer::getInstance()->loop(2 * 1000, function () {
                         //为了防止因为任务阻塞，引起定时器不准确，把任务给异步进程处理
-                        echo 'bbbbb';echo '\n';
+                        echo 'bbbbb';echo "\n";
                     });
                     Timer::getInstance()->after(3 * 1000, function () {
                         //为了防止因为任务阻塞，引起定时器不准确，把任务给异步进程处理
-                        echo 'ccccc';echo '\n';
+                        echo 'ccccc';echo "\n";
                     });
                 });
             }
