ee16d6724a1b173d10c1cc0fd4707c37b81f9144
LCR <994094256@qq.com>
2019-04-26 17:58:14 +0800

    fix

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -101,7 +101,7 @@ class EasySwooleEvent implements Event
 //                });
 //            };
             if ($workerId == 1){
-                Timer::getInstance()->loop(4 * 1000,function (){
+                Timer::getInstance()->loop(2 * 1000,function (){
                     $ret = MysqlPool::invoke(function (MysqlObject $db){
                         $gmember = new GameMemberModel($db);
                         $ret = $gmember->selectAll(1000);
